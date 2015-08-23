af7452f20857a9161324aaa0255790c40ba5c585
Michael McDevitt <mmcdevi1@gmail.com>
2015-08-22 22:42:02 -0400

    widget

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,19 +1,22 @@
 class Listing < ActiveRecord::Base
-  if Rails.env.development?
-    has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg"
-  else
-    has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg",
-                      :storage => :dropbox,
-                      :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
-                      :path => ":style/:id_:filename"
-  end
-
   validates :name, :description, :price, presence: true
   validates :price, numericality: { greater_than: 0 }
-  validates_attachment_presence :image
 
   belongs_to :user
   has_many :order_items
 
   scope :not_sold, -> { where(active: true) }
+
+  has_attached_file :image, :default_url => "/assets/office1.jpg", styles: {
+    large: "300x200#",
+    medium: "300x300#",
+    small: "150x150#",
+    thumb: "140x140#"
+
+  }
+  # ,
+  # :storage => :s3,
+  # :bucket => ENV['LEASIFY'],
+  # :s3_credentials => File.join(Rails.root, 'config', 's3.yml')
+  validates_attachment_content_type :image, :content_type => /\Aimage/
 end
