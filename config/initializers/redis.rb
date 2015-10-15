d603770f4e403d9b8fff705d6f1f81971e40f2a4
Michael McDevitt <mmcdevi1@gmail.com>
2015-10-14 21:10:21 -0400

    redis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,1 @@
-uri = URI.parse(ENV["REDISTOGO_URL"])
-REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-Resque.redis = REDIS
+# heroku config:set REDIS_PROVIDER=REDISTOGO_URL
