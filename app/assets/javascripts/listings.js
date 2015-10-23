761ec37fe85c4300a7c5fb54fd5af33429eb31d4
Michael McDevitt <mmcdevi1@gmail.com>
2015-10-23 17:13:24 -0400

    reg fixes on production

--- app/assets/javascripts/listings.js
+++ app/assets/javascripts/listings.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+  var $addedToCartBtn = $('#added_to_cart_btn');
+
   $('#selling_price').keyup(function() {
     $('#buyer_pays').text('$' + Math.round($(this).val() * 100) / 100);
     $('#earnings').text('$' + Math.round($(this).val() * 0.90 * 100) / 100);
@@ -6,10 +8,11 @@ $(document).ready(function() {
 
   $('input[type=radio]:checked').parent().addClass('active')
 
-  $('#go_to_cart_btn').hide();
+  // Hide 'add to cart' btn when submitted. Replace with 'Go to cart' btn.
+  $addedToCartBtn.hide();
   $('#submit-btn').click(function() {
     $(this).hide();
-    $('#go_to_cart_btn').show();
+    $addedToCartBtn.show();
   });
 });
 
