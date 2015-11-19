08c2ecac43ab893d404645817047e8c5e5ffbe2a
Michael McDevitt <mmcdevi1@gmail.com>
2015-11-19 18:24:32 -0500

    fix gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,9 +85,10 @@ Thrice::Application.configure do
   config.action_mailer.smtp_settings = {
     :address => 'smtp.gmail.com',
     :port    => 587,
+    :domain => 'gmail.com',
     :user_name => ENV['gmail_username'],
     :password  => ENV['gmail_password'],
-    :authentication => 'plain',
+    :authentication => :login,
     :enable_starttls_auto => true,
   }
 
