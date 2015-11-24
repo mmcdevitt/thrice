8985e2c8596450ca0d5beb7ddc5ed3ac9a9a43e9
Michael McDevitt <mmcdevi1@gmail.com>
2015-11-24 15:23:27 -0500

    post

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,20 +4,20 @@ class Post < ActiveRecord::Base
 
   belongs_to :user
 
-  has_attached_file :post_image, :default_url => "assets/thrice-logo-bg.jpg", styles: {
-    large: "650x500#",
-    medium: "300x300#",
-    small: "150x150#",
-    thumb: "140x140#"
-  },
-  :storage => :s3,
-  :bucket => ENV['S3_BUCKET_NAME'],
-  :s3_credentials => {
-    :access_key_id => ENV['S3_ACCESS_KEY'],
-    :secret_access_key => ENV['S3_SECRET_KEY']
-  }
+  # has_attached_file :post_image, :default_url => "assets/thrice-logo-bg.jpg", styles: {
+  #   large: "650x500#",
+  #   medium: "300x300#",
+  #   small: "150x150#",
+  #   thumb: "140x140#"
+  # },
+  # :storage => :s3,
+  # :bucket => ENV['S3_BUCKET_NAME'],
+  # :s3_credentials => {
+  #   :access_key_id => ENV['S3_ACCESS_KEY'],
+  #   :secret_access_key => ENV['S3_SECRET_KEY']
+  # }
 
-  validates_attachment_content_type :post_image, :content_type => /\Aimage/
+  # validates_attachment_content_type :post_image, :content_type => /\Aimage/
 
   def format_markdown
     markdown(self.content)
