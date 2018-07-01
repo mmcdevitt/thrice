f3fae35f1e1ef67b66f4ccde0acdfa218a9fe899
mmcdevi1 <mmcdevi1@gmail.com>
2018-07-01 18:37:20 -0400

    cool

---
+++ routes.js
@@ -0,0 +1,49 @@
+Michaels-MacBook-Air-3:express_tut mmcdevi1$ touch index.js
+Michaels-MacBook-Air-3:express_tut mmcdevi1$ npm install express
+npm notice created a lockfile as package-lock.json. You should commit this file.
+npm WARN express_tut@1.0.0 No description
+npm WARN express_tut@1.0.0 No repository field.
+
++ express@4.16.3
+added 50 packages in 4.4s
+
+
+   ╭─────────────────────────────────────╮
+   │                                     │
+   │   Update available 5.6.0 → 6.0.0    │
+   │     Run npm i -g npm to update      │
+   │                                     │
+   ╰─────────────────────────────────────╯
+
+Michaels-MacBook-Air-3:express_tut mmcdevi1$ ls
+index.js		package-lock.json
+node_modules		package.json
+Michaels-MacBook-Air-3:express_tut mmcdevi1$ nodemon index
+[nodemon] 1.17.3
+[nodemon] to restart at any time, enter `rs`
+[nodemon] watching: *.*
+[nodemon] starting `node index index.js`
+Listening on port 3000
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
